e29b52da09d12de209d142c97bcbb24d4189d3be
jobidlv <82426798+jobidlv@users.noreply.github.com>
2024-07-01 17:22:03 -0400

    chatper 4 done

--- src/Ch04/04_05/CodeDemo.cpp
+++ src/Ch04/04_05/CodeDemo.cpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include "records.h"
 
+#define DEBUGGING
+
 int main(){
     float GPA = 0.0f;
     int id;
@@ -26,9 +28,38 @@ int main(){
 
     // Calculate the GPA for the selected student.
     // Write your code here
+    float credits = 0.0f;
+    for(int i = 0; i < grades.size(); i++){
+        if(grades[i].get_student_id() == id){
+            #ifdef DEBUGGING
+            std::cout << "ID: " << grades[i].get_student_id() << std::endl;
+            #endif
+            switch(grades[i].get_grade()){
+                case 'A':
+                    GPA += 4.0*courses[grades[i].get_course_id()-1].get_credits(); break;
+                case 'B':
+                    GPA += 3.0*courses[grades[i].get_course_id()-1].get_credits(); break;
+                case 'C':
+                    GPA += 2.0*courses[grades[i].get_course_id()-1].get_credits(); break;
+                case 'D':
+                    GPA += 1.0*courses[grades[i].get_course_id()-1].get_credits(); break;
+                case 'F':
+                    GPA += 0.0; break;
+                default:
+                    GPA += 0.0; break;
+            }
+            credits += (float)courses[grades[i].get_course_id()-1].get_credits();
+        }
+    }
+    #ifdef DEBUGGING
+    std::cout <<"Total earned credits: " << GPA << std::endl;
+    std::cout << "Total number of credits: " << credits << std::endl;
+    #endif
+
+    GPA /= credits;
 
     std::string student_str;
-    student_str = students[0].get_name(); // Change this to the selected student's name
+    student_str = students[id-1].get_name(); // Change this to the selected student's name
 
     std::cout << "The GPA for " << student_str << " is " << GPA << std::endl;
     
